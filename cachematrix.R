784636b02b27fd8e067fd146f82350ce0d9c7533
Fjd <diazfj1@gmail.com>
2015-10-22 11:58:55 -0400

    Assignment #2
    Changes to cahematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function sets the inverse of a matrix "m" and stores it in "setmatrix" as part of a list.
 
 makeCacheMatrix <- function(x = matrix()) {
+		m <- NULL
+        	set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+	get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
+}
+
+## This function retreives m stored in getmatrix and if m is NOT =null then returns m.
+## If m is null, then m is recalculated using the solve function and stored again in "setmatrix"
 
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmaterix(m)
+        m
 }
